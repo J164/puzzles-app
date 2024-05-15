ea85f2390da7219c881b30997f96dc50fc226c64
Jacob Edley <jac.dan@icloud.com>
2024-05-15 18:16:32 -0500

    split app into pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-import 'dart:convert';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-
-import 'package:http/http.dart' as http;
+import 'package:puzzle_app/pages/create.dart';
+import 'package:puzzle_app/pages/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -20,150 +17,37 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Puzzle App'),
+      home: const Container(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Container extends StatefulWidget {
+  const Container({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Container> createState() => _ContainerState();
 }
 
-class Puzzle {
-  final String unsolved;
-  final String solved;
-
-  const Puzzle({
-    required this.unsolved,
-    required this.solved,
-  });
-
-  factory Puzzle.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'solved': String solved,
-        'unsolved': String unsolved,
-      } =>
-        Puzzle(
-          unsolved: unsolved,
-          solved: solved,
-        ),
-      _ => throw const FormatException('Failed to load puzzle'),
-    };
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _selectedPuzzle = 'maze';
-  bool _showSolution = false;
-  Image? _unsolved;
-  Image? _solved;
-
-  void _puzzleSelectCallback(Object? selectedPuzzle) {
-    if (selectedPuzzle is String) {
-      setState(() {
-        _selectedPuzzle = selectedPuzzle;
-      });
-    }
-  }
-
-  void _toggleSolution() {
-    setState(() {
-      _showSolution = !_showSolution;
-    });
-  }
-
-  Future<http.Response> _fetchPuzzle() {
-    return http
-        .get(Uri.parse('http://127.0.0.1:8080/$_selectedPuzzle?width=50'));
-  }
-
-  Future<void> _generatePuzzle() async {
-    http.Response res = await _fetchPuzzle();
-
-    if (res.statusCode == 200) {
-      var Puzzle(:unsolved, :solved) = Puzzle.fromJson(jsonDecode(res.body));
-
-      setState(() {
-        _showSolution = false;
-        _unsolved = Image.network(
-            'https://puzzles-db.j164.workers.dev/api/images/?key=$unsolved');
-        _solved = Image.network(
-            'https://puzzles-db.j164.workers.dev/api/images/?key=$solved');
-      });
-    } else {
-      throw Exception('Failed to fetch puzzle');
-    }
-  }
+class _ContainerState extends State<Container> {
+  int index = 0;
+  final pages = [const HomePage(), const CreatePage()];
 
   @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
-
-    double imageSize = min(screenHeight, screenWidth) * 0.6;
-
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const Text(
-              "Generate a puzzle",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Puzzle Type:',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(width: 10),
-                DropdownButton(items: const [
-                  DropdownMenuItem(value: 'maze', child: Text('Maze')),
-                  // DropdownMenuItem(value: 'nonogram', child: Text('Nonogram')),
-                  // DropdownMenuItem(value: 'sudoku', child: Text('Sudoku')),
-                ], value: _selectedPuzzle, onChanged: _puzzleSelectCallback)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: imageSize,
-                  height: imageSize,
-                  child: _showSolution ? _solved : _unsolved,
-                ),
-              ],
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              ElevatedButton(
-                  onPressed: _generatePuzzle,
-                  child: const Text(
-                    "Generate Puzzle",
-                    style: TextStyle(fontSize: 20),
-                  )),
-              const SizedBox(
-                width: 20,
-              ),
-              ElevatedButton(
-                  onPressed: _unsolved is Image ? _toggleSolution : null,
-                  child: Text(
-                    _showSolution ? "Hide Solution" : "Show Solution",
-                    style: const TextStyle(fontSize: 20),
-                  )),
-            ]),
-          ],
-        )));
-  }
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Puzzle App'),
+      ),
+      body: pages[index],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected: (index) => setState(() {
+          this.index = index;
+        }),
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.add), label: 'Create'),
+        ],
+      ));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:puzzle_app/main.dart';
-
 void main() {}
