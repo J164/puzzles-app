5d0d411b11bf94ed89a1e32ff641ae885c65693b
Jacob Edley <jac.dan@icloud.com>
2024-05-14 21:19:10 -0500

    improve styling ans switch to new api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -45,12 +46,12 @@ class Puzzle {
   factory Puzzle.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-        'solved': List<dynamic> solved,
-        'unsolved': List<dynamic> unsolved,
+        'solved': String solved,
+        'unsolved': String unsolved,
       } =>
         Puzzle(
-          unsolved: unsolved[0],
-          solved: solved[0],
+          unsolved: unsolved,
+          solved: solved,
         ),
       _ => throw const FormatException('Failed to load puzzle'),
     };
@@ -59,8 +60,9 @@ class Puzzle {
 
 class _MyHomePageState extends State<MyHomePage> {
   String _selectedPuzzle = 'maze';
-  Image _unsolved = const Image(image: AssetImage("assets/missing.png"));
-  Image _solved = const Image(image: AssetImage("assets/missing.png"));
+  bool _showSolution = false;
+  Image? _unsolved;
+  Image? _solved;
 
   void _puzzleSelectCallback(Object? selectedPuzzle) {
     if (selectedPuzzle is String) {
@@ -70,20 +72,29 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void _toggleSolution() {
+    setState(() {
+      _showSolution = !_showSolution;
+    });
+  }
+
   Future<http.Response> _fetchPuzzle() {
     return http
-        .get(Uri.parse('http://127.0.0.1:3000/$_selectedPuzzle?width=10'));
+        .get(Uri.parse('http://127.0.0.1:8080/$_selectedPuzzle?width=50'));
   }
 
   Future<void> _generatePuzzle() async {
     http.Response res = await _fetchPuzzle();
 
     if (res.statusCode == 200) {
-      Puzzle puzzle = Puzzle.fromJson(jsonDecode(res.body));
+      var Puzzle(:unsolved, :solved) = Puzzle.fromJson(jsonDecode(res.body));
 
       setState(() {
-        _unsolved = Image.network(puzzle.unsolved);
-        _solved = Image.network(puzzle.solved);
+        _showSolution = false;
+        _unsolved = Image.network(
+            'https://puzzles-db.j164.workers.dev/api/images/?key=$unsolved');
+        _solved = Image.network(
+            'https://puzzles-db.j164.workers.dev/api/images/?key=$solved');
       });
     } else {
       throw Exception('Failed to fetch puzzle');
@@ -92,36 +103,67 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    double imageSize = min(screenHeight, screenWidth) * 0.6;
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Puzzle Type:',
-            ),
-            DropdownButton(items: const [
-              DropdownMenuItem(value: 'maze', child: Text('Maze')),
-              // DropdownMenuItem(value: 'nonogram', child: Text('Nonogram')),
-              // DropdownMenuItem(value: 'sudoku', child: Text('Sudoku')),
-            ], value: _selectedPuzzle, onChanged: _puzzleSelectCallback)
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [_unsolved, _solved],
-        )
-      ])),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _generatePuzzle,
-        tooltip: 'Generate',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              "Generate a puzzle",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Puzzle Type:',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(width: 10),
+                DropdownButton(items: const [
+                  DropdownMenuItem(value: 'maze', child: Text('Maze')),
+                  // DropdownMenuItem(value: 'nonogram', child: Text('Nonogram')),
+                  // DropdownMenuItem(value: 'sudoku', child: Text('Sudoku')),
+                ], value: _selectedPuzzle, onChanged: _puzzleSelectCallback)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: imageSize,
+                  height: imageSize,
+                  child: _showSolution ? _solved : _unsolved,
+                ),
+              ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  onPressed: _generatePuzzle,
+                  child: const Text(
+                    "Generate Puzzle",
+                    style: TextStyle(fontSize: 20),
+                  )),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                  onPressed: _unsolved is Image ? _toggleSolution : null,
+                  child: Text(
+                    _showSolution ? "Hide Solution" : "Show Solution",
+                    style: const TextStyle(fontSize: 20),
+                  )),
+            ]),
+          ],
+        )));
   }
 }
